ebd98888d1f5141d54744c8f7882bdfc5a4b08a1
Chris Dell <cdell@ufl.edu>
2017-04-10 22:15:01 -0400

    rough configuration(), added constructor with config string, and json string

--- WeatherWidget.cpp
+++ WeatherWidget.cpp
@@ -3,11 +3,102 @@
 #include "httpreq.h"
 #include "jsonhttpreq.h"
 
-void WeatherWidget::configure()
+
+
+/****************************
+ Constructors
+ ____________________________
+ ****************************/
+
+WeatherWidget::WeatherWidget(){}
+
+/************
+ * config string parameter should be the whole configuration, like this:
+ * {
+ *      "name": "Weather",
+ *      "configuration": {
+ *          "zip": #####,
+ *          "refreshInterval": ###  //(in seconds)
+ *      }
+ * }
+ * @param config
+ */
+
+WeatherWidget::WeatherWidget(std::string config)
 {
-    setZipCode("32601");
+    nlohmann::json configJson = nlohmann::json::parse(config);
+    setZipCode(configJson["configuration"]["zip"]);
+    setRefreshInterval(configJson["configuration"]["refreshInterval"]);
 }
 
+/*********
+ * config parameter should be only the "configuration" section of
+ * the json:
+ *      { "zip": #####, "refreshInterval": ### }
+ *
+ * @param config
+ */
+WeatherWidget::WeatherWidget(nlohmann::json config)
+{
+    setZipCode(config["zip"]);
+    setRefreshInterval(config["refreshInterval"]);
+}
+
+
+
+
+
+
+
+
+/****************************
+ Public
+ ___________________________
+ ****************************/
+void WeatherWidget::configure()
+{
+    std::string zip = "";
+    std::cout << "Please configure the Weather Widget." << std::endl;
+    std::cout << "What is your zipcode?" << std::endl;
+    std::cout << "Zipcode: ";
+    std::getline(std::cin, zip);
+    while (!validZipCode(zip))
+    {
+        std::cout << "What is your zipcode?" << std::endl;
+        std::cout << "Zipcode: ";
+        std::getline(std::cin, zip);
+    }
+
+    setZipCode(zip);
+}
+
+
+//returns string representation of configuration,
+//used to create config file
+std::string WeatherWidget::getConfiguration()
+{
+    nlohmann::json configurationJson = getConfigurationJson();
+    return configurationJson.dump(4);
+}
+
+//returns json representation of configuration,
+//used to create config file
+nlohmann::json WeatherWidget::getConfigurationJson()
+{
+    nlohmann::json configurationJson = {
+            {"name", "Weather"},
+            {"configuration", {
+                             {"zip", getZipCode()},
+                             {"refreshInterval", getRefreshInterval()}
+                     }}
+    };
+
+    return configurationJson;
+}
+
+//returns string representation of weather results
+//
+//Uses the openweather API and then transforms into subset of information
 std::string WeatherWidget::refreshData()
 {
     JSONHTTPReq req = JSONHTTPReq();
@@ -19,21 +110,86 @@ std::string WeatherWidget::refreshData()
     return transformedJson.dump(4);
 }
 
-std::string WeatherWidget::getConfiguration()
+
+
+
+
+
+
+
+/****************************
+ Private
+ ___________________________
+ ****************************/
+
+//returns URL for daily weather forecast
+std::string WeatherWidget::getDailyUrl(std::string zip)
 {
-    return "";
+    return ENDPOINT + "?zip=" + zip + ",us&appid=" + APIKEY;
 }
+
+//returns URL for 5 day forecast
+std::string WeatherWidget::get5dayUrl(std::string zip)
+{
+    //todo: do this
+    return "todo";
+}
+
+/************************
+ *
+ * @param zip
+ * @return bool validity of zip code
+ */
+bool WeatherWidget::validZipCode(std::string zip)
+{
+    if (zip.length() != 5)
+    {
+        std::cout << "Sorry, zip codes must be 5 digits." << std::endl;
+        return false;
+    }
+    for (int i = 0; i < zip.length(); ++i)
+    {
+        int charcode = zip[i] + 0;
+        if (charcode < 48 || charcode > 57)
+        {
+            std::cout << "Sorry, zip code must contain only digits" << std::endl;
+            return false;
+
+        }
+    }
+    //we know the zip is now 5 digits, so let's check if it's valid
+    //in the weather api
+    JSONHTTPReq req = JSONHTTPReq();
+    std::string url = getDailyUrl(zip);
+    req.setUrl(url);
+    req.send();
+    nlohmann::json json = req.getJSONResponse();
+    if (json["cod"] == "200" || json["cod"] == 200)
+        return true;
+    if (json["cod"] == "404" || json["cod"] == 404)
+        std::cout << "Error: bad zip code, " << json["message"] << std::endl;
+    return false;
+}
+
 
 std::string WeatherWidget::getZipCode()
 {
     return zipcode;
 }
 
+
 void WeatherWidget::setZipCode(std::string zip)
 {
     zipcode = zip;
 }
 
+/*****************
+ * Transforms the response from the openweather api request into
+ * the useful information we need, and returns it in json format
+ *
+ * @param response
+ * @return json
+ */
 nlohmann::json WeatherWidget::transformResponse(nlohmann::json response)
 {
     int currentTemp = response["main"]["temp"];
--- WeatherWidget.h
+++ WeatherWidget.h
@@ -10,18 +10,28 @@
 
 class WeatherWidget : public Widget {
 public:
-    std::string getZipCode();
-    void setZipCode(std::string zip);
+    WeatherWidget();
+    WeatherWidget(std::string config);
+    WeatherWidget(nlohmann::json config);
+
     void configure();
     std::string getConfiguration();
+    nlohmann::json getConfigurationJson();
     std::string refreshData();
 
 private:
-
     std::string zipcode;
     std::string const ENDPOINT = "http://api.openweathermap.org/data/2.5/weather";
     std::string const APIKEY = "edc7afc82b7d745a80be3761e51b3055";
+
+
+    std::string getZipCode();
+    void setZipCode(std::string zip);
     nlohmann::json transformResponse(nlohmann::json response);
+    std::string getDailyUrl(std::string zip);
+    std::string get5dayUrl(std::string zip);
+    void configureZipCode();
+    bool validZipCode(std::string zip);
 
 };
 
